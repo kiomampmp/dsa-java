e6aac69643e043d27b664cdf9c8d4ce7c3573e71
kiomampmp <kioma-mp@proton.me>
2026-06-12 23:28:49 -0700

    added basic methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Date;
+
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello World!");
+        Date fecha = new Date();
     }
 }
---
+++ src/dsa/StackPractice.java
@@ -0,0 +1,33 @@
+package dsa;
+
+import java.util.ArrayList;
+
+public class StackPractice {
+    private ArrayList<String> data;
+
+    public void push (String element){
+        data.add(element);
+
+
+    }
+    public void peek {
+        System.out.println(data.get(-1));
+    }
+
+    public String pop{
+        String result = toString(data.get(-1));
+        return result;
+    }
+
+    public boolean isEmpty{
+        System.out.println("Hello World");
+    }
+
+    public boolean isFull{
+        System.out.println("Hello World");
+    }
+
+
+
+
+}
